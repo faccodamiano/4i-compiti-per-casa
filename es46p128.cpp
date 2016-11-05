dc24c81b4d5c73199b428b66ff9a8924654d4cd1
faccodamiano <faccodamiano@gmail.com>
2016-11-05 11:57:09 +0100

    ho aggiunto codici

---
+++ es46p128.cpp
@@ -0,0 +1,15 @@
+//p.128 n 46
+#inlude <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    float angolo;
+     cout << "inserisci un angolo: " >> endl ;
+     cin >> angolo ;
+     return 0 ;
+
+
+
+
+}
