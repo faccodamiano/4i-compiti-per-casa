e6f103e5952ca8b09c01d9f055d0e2e0abd6fb48
faccodamiano <faccodamiano@gmail.com>
2016-11-25 18:58:52 +0100

    Add files via upload

---
+++ es34pag 127.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void numerointero (int x)
+
+int main {
+
+    int valore;
+    cin>> valore;
+    numerointero (valore);
+
+    return 0;
+}
+void numerointero (int x) {
+    int lista, n1, n2, n3, n4 ;
+    enum lista {x, n1, n2, n3, n4};
+    cout<< "lista" << endl;
+
+}
